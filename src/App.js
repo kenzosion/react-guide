4d44d6279c2368a393608588b022f5a3c55553da
Haur Kang <haur.kang@gmail.com>
2019-01-13 22:24:49 +0000

    test state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Hiya', age: 28 },
+      { name: 'Hiyo', age: 38 },
+      { name: 'Hiyi', age: 48 },
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>HI THERE</h1>
         <p>This is working woohoo, test this out { Math.floor(Math.random() * 30)}</p>
-        <Person name='testing' />
+        <Person name={this.state.persons[0].name} />
         <Person name='testing123'> My hobby is: coding</Person>
 
       </div>
