fce40af5e7463acdbdde15447393da3d81176161
Haur Kang <haur.kang@gmail.com>
2019-01-20 22:46:56 +0000

    creates Counter component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import Counter from './components/counter';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
